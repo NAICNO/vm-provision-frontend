43b84204d44550531960c121d80cda62aadb9f29
Ashen <ashenrandika@gmail.com>
2024-01-25 14:11:12 +0100

    Update Vite. Changes to home screen. Addind NAIC logos.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,15 @@
-import { Button, Center, Container, Heading, VStack } from '@chakra-ui/react'
+import {
+  Button,
+  Container,
+  Heading,
+  Image,
+  VStack,
+  Text,
+  Link,
+  Box,
+  Card,
+  CardBody, Flex, IconButton, useColorMode
+} from '@chakra-ui/react'
 import {
   AUTH_CLIENT_ID,
   AUTH_OPEN_ID_SCOPES,
@@ -8,8 +19,14 @@ import {
 } from '../constants/Constants.ts'
 import { generateRandomString } from '../util'
 import EducloudIcon from '../components/EducloudIcon.tsx'
+import { MoonIcon, SunIcon } from '@chakra-ui/icons'
 
 export default function Home() {
+
+  const {colorMode, toggleColorMode} = useColorMode()
+
+  const naicLogo = colorMode === 'light' ? 'naic/naic_logo_horizontal_dark.svg' : 'naic/naic_logo_horizontal_light.svg'
+
   const generateState = () => {
     return generateRandomString()
   }
@@ -26,7 +43,12 @@ export default function Home() {
     const state = generateState()
     const nonce = generateNonce()
 
-    const url = `${AUTH_URL}?client_id=${AUTH_CLIENT_ID}&redirect_uri=${AUTH_REDIRECT_URL}&response_type=${AUTH_RESPONSE_TYPE}&scope=${AUTH_OPEN_ID_SCOPES}&state=${state}&nonce=${nonce}`
+    const url = `${AUTH_URL}?client_id=${AUTH_CLIENT_ID}
+    &redirect_uri=${AUTH_REDIRECT_URL}
+    &response_type=${AUTH_RESPONSE_TYPE}
+    &scope=${AUTH_OPEN_ID_SCOPES}
+    &state=${state}
+    &nonce=${nonce}`
 
     return encodeURI(url)
   }
@@ -37,22 +59,57 @@ export default function Home() {
   }
 
   return (
-    <Container maxW="xl" height="100vh" centerContent>
-      <Center height="100%">
-        <VStack spacing={6} width="100%" maxW="md" padding="4">
-          <Heading as="h2" size="lg">Log in to your account</Heading>
-          <Button
-            width="full"
-            colorScheme="gray"
-            mt={4}
-            onClick={handleLogin}
-            variant={'outline'}
-            leftIcon={<EducloudIcon/>}
-          >
-            Continue with Educloud
-          </Button>
-        </VStack>
-      </Center>
-    </Container>
+    <Flex direction="column" minHeight="100vh"> {/* Full viewport height and flex column */}
+
+      <Container as="main" flex="1" maxW="lg" py={{base: '12', md: '24'}} px={{base: '0', sm: '8'}}>
+        <Card>
+          <CardBody>
+            <VStack spacing={6} width="100%" maxW="md" padding="4">
+              <Image
+                src={naicLogo}
+                alt="NAIC logo"
+                width="500px"
+                mb={4}
+              />
+              <Heading size={{base: 'md', md: 'lg'}}>
+                Self Service VM Portal
+              </Heading>
+              <Text size={{base: 'sm', md: 'md'}}>
+                Log in to your account
+              </Text>
+              <Button
+                width="full"
+                colorScheme="gray"
+                onClick={handleLogin}
+                variant={'outline'}
+                leftIcon={<EducloudIcon/>}
+              >
+                Continue with Educloud
+              </Button>
+            </VStack>
+          </CardBody>
+        </Card>
+      </Container>
+      <Box as="footer" py="4" mt="8" textAlign="center">
+        <Text>
+          Visit our main website at{' '}
+          <Link href="http://www.naic.no" isExternal color="blue.500">
+            www.naic.no
+          </Link>
+        </Text>
+      </Box>
+      <IconButton
+        icon={colorMode === 'light' ? <MoonIcon/> : <SunIcon/>}
+        isRound={true}
+        size="md"
+        position="fixed"
+        bottom={4}
+        right={4}
+        onClick={toggleColorMode}
+        aria-label={`Switch to ${colorMode === 'light' ? 'dark' : 'light'} mode`}
+        opacity="0.4" // Reduced opacity
+        _hover={{ opacity: 1 }} // Full opacity on hover
+      />
+    </Flex>
   )
 }
