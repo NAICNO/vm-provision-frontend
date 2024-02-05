d855f67cc422b3fcfbbe5a73d658e564c800dfd4
Ashen <ashenrandika@gmail.com>
2024-02-05 15:17:26 +0100

    Code clean up

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,9 +1,4 @@
 import {
-  CheckCircleIcon,
-  InfoIcon,
-  WarningTwoIcon
-} from '@chakra-ui/icons'
-import {
   Tabs,
   TabList,
   Tab,
@@ -25,7 +20,6 @@ export default function Profile() {
     <Tabs variant="enclosed">
       <TabList>
         <Tab _selected={ { color: 'white', bg: 'blue.400' } }>Account Info</Tab>
-        <Tab _selected={ { color: 'white', bg: 'blue.400' } }>Activity History</Tab>
       </TabList>
       <TabPanels py="10px">
         <TabPanel>
@@ -37,34 +31,6 @@ export default function Profile() {
             <ListItem>
               <ListIcon as={ MdAlternateEmail }/>
               Email: {`${user?.email}`}
-            </ListItem>
-          </List>
-        </TabPanel>
-        <TabPanel>
-          <List spacing={ 4 }>
-            <ListItem>
-              <ListIcon as={ CheckCircleIcon } color="blue.400"/>
-              VM creation requested.
-            </ListItem>
-            <ListItem>
-              <ListIcon as={ CheckCircleIcon } color="green.400"/>
-              VM creation successful.
-            </ListItem>
-            <ListItem>
-              <ListIcon as={ WarningTwoIcon } color="red.400"/>
-              VM creation failed.
-            </ListItem>
-            <ListItem>
-              <ListIcon as={ CheckCircleIcon } color="teal.400"/>
-              VM deletion successful.
-            </ListItem>
-            <ListItem>
-              <ListIcon as={ InfoIcon } color="blue.400"/>
-              Quota increase requested.
-            </ListItem>
-            <ListItem>
-              <ListIcon as={ CheckCircleIcon } color="green.400"/>
-              VM creation successful.
             </ListItem>
           </List>
         </TabPanel>
