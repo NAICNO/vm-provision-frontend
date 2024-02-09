d6cf83b0caed0d6ecc9fb6ae713ec843d4cef2b3
Ashen <ashenrandika@gmail.com>
2024-02-09 16:27:35 +0100

    Setting configurations to automatically drop console log and other debugger statements from production build.

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,27 @@
 import { sentryVitePlugin } from '@sentry/vite-plugin'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import strip from '@rollup/plugin-strip'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({mode}) => ({
   plugins: [
     react(),
     sentryVitePlugin({
       org: 'uio-naic',
       project: 'vm-frontend'
     }),
-    strip({
-      debugger: process.env.NODE_ENV === 'production',
-    })
   ],
 
   server: {
     port: 8080,
   },
 
+  esbuild: {
+    drop: mode === 'production' ? ['console', 'debugger'] : []
+  },
+
   build: {
     sourcemap: true
   },
 
-})
+}))
