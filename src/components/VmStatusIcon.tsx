d926542c678ffb82ede912aca9b87e695662be3d
ashenr <ashenrandika@gmail.com>
2024-04-05 15:55:06 +0200

    Adding new state "Initializing" to indicate the initial software installation process.

--- src/components/VmStatusIcon.tsx
+++ src/components/VmStatusIcon.tsx
@@ -1,9 +1,9 @@
-import { VmStatusType } from '../types/VmStatusType.ts'
-import { statusColorMap } from '../util'
-import { Icon } from '@chakra-ui/icons'
-import { MdPlayCircleOutline, MdStopCircle } from 'react-icons/md'
-import { CircularProgress } from '@chakra-ui/react'
-import { IoMdInformationCircleOutline } from 'react-icons/io'
+import {VmStatusType} from '../types/VmStatusType.ts'
+import {statusColorMap} from '../util'
+import {Icon} from '@chakra-ui/icons'
+import {MdPlayCircleOutline, MdStopCircle} from 'react-icons/md'
+import {CircularProgress} from '@chakra-ui/react'
+import {IoMdInformationCircleOutline} from 'react-icons/io'
 
 export const VmStatusIcon = ({status}: { status: VmStatusType }) => {
   const color = statusColorMap[status]
@@ -13,6 +13,10 @@ export const VmStatusIcon = ({status}: { status: VmStatusType }) => {
   case VmStatusType.RUNNING:
     return <Icon as={MdPlayCircleOutline}/>
   case VmStatusType.PROVISIONING:
+    return <CircularProgress isIndeterminate size="15px" color={color}/>
+  case VmStatusType.PROVISIONING_COMPLETED:
+    return <Icon as={IoMdInformationCircleOutline}/>
+  case VmStatusType.INITIALIZING:
     return <CircularProgress isIndeterminate size="15px" color={color}/>
   case VmStatusType.DESTROYING:
     return <CircularProgress isIndeterminate size="15px" color={color}/>
